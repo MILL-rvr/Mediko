853ceba5829ac47bb43fd8a2af27b80b6425d4de
MILL-rvr <kmriveraacads@gmail.com>
2025-11-28 16:10:22 +0800

    Updated code

--- main/user/user_SaveProfile.php
+++ main/user/user_SaveProfile.php
@@ -4,7 +4,7 @@ include("../../config/database.php");
 
 $user_id = $_SESSION['user_id'] ?? 0;
 if (!$user_id) {
-    header("Location: ../login.php"); 
+    header("Location:index.php"); 
     exit;
 }
 
