412dcf018b693956e9558e09cf78b6cba7a91214
MILL-rvr <kmriveraacads@gmail.com>
2025-11-28 16:57:55 +0800

    Update DB connection for Render

--- config/database.php
+++ config/database.php
@@ -1,12 +1,25 @@
+<!-- <?php
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'db_project_sia');
+// $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// if($conn === false){
+//     die("ERROR: Could not connect. " . mysqli_connect_error());
+// }
+?> -->
+
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'db_project_sia');
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = mysqli_connect(
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_NAME'),
+    getenv('DB_PORT')  // usually 3306
+);
 
-if($conn === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
-
 ?>
