84614055705a9163ed5e931bdc6d45b1b45fd52b
MILL-rvr <kmriveraacads@gmail.com>
2025-11-28 17:25:14 +0800

    Fix DB connection for Render

--- config/database.php
+++ config/database.php
@@ -1,15 +1,3 @@
-<!-- <?php
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_NAME', 'db_project_sia');
-// $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-// if($conn === false){
-//     die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
-?> -->
-
 <?php
 $host = getenv('DB_HOST') ?: 'localhost';
 $user = getenv('DB_USER') ?: 'root';
